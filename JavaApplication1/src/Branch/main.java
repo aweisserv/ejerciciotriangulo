8735884165d087718365ebb2f2d8f9cb6fd00a63
Maggi <Maggi@DESKTOP-7SGCSO2>
2016-05-31 23:28:45 -0300

    prueba

---
+++ JavaApplication1/src/Branch/main.java
@@ -0,0 +1,31 @@
+package Branch;
+import Trunk.*;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+			ingresarlados();
+	}
+		
+//-----------Methods-----------
+private static void ingresarlados(){
+	int lado1, lado2, lado3;
+	Scanner sc = new Scanner(System.in);
+	Triangulo T = new Triangulo();
+		
+	System.out.print("Introduzca Lado 1: \n");
+	lado1 = sc.nextInt();
+	T.ValidacionTriangulo(lado1);
+	System.out.print("Introduzca Lado 2: \n");
+	lado2 = sc.nextInt();
+	T.ValidacionTriangulo(lado2);
+	System.out.print("Introduzca Lado 3: \n");
+	lado3 = sc.nextInt();
+	T.ValidacionTriangulo(lado3);
+		
+	T.ValidacionTriangulo2(lado1, lado2, lado3);
+	T.TipoTriangulo(lado1, lado2, lado3);
+	sc.close();				
+}
+}
+
